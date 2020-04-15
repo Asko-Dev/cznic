8c52d5d02f43a9b5f20ac839257b3c3ac8cda213
Adela Skotak <skotak.adela@gmail.com>
2020-04-15 11:37:21 +0200

    Models, views, url path, templates

--- domains/models.py
+++ domains/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+# Flag model, Flags will be assigned to Domains
+class DomainFlag(models.Model):
+    """Domain Flad for Domain"""
+
+    TYPE_CHOICE = [
+        ('EXPIRED', 'Expired'),
+        ('OUTZONE', 'Outzone'),
+        ('DELETE_CANDIDATE', 'Delete Candidate')
+    ]
+    type = models.CharField(
+        choices=TYPE_CHOICE,
+        max_length=20,
+        default='EXPIRED'
+    )
+    flag_valid_from = models.DateTimeField()
+    flag_valid_to = models.DateTimeField()
+
+    REQUIRED_FIELDS = ('type', 'flag_valid_from', 'flag_valid_to',)
+
+    def __str__(self):
+        return self.type
+
+
+# main domain model to which flags will get assigned
+class Domain(models.Model):
+    """Main Domain model"""
+    FQDN = models.CharField(
+        max_length=255,
+        help_text='Fully Qualified Domain Name',
+        unique=True,
+        validators=[RegexValidator(
+            '^www+\.[a-z0-9]+\.[a-z0-9]{1,4}$',
+            'www.yourdomain.com only',
+            'Invalid Entry')
+        ]
+    )
+    date_created = models.DateTimeField()
+    date_expires = models.DateTimeField()
+    date_deletion = models.DateTimeField()
+    flag = models.ManyToManyField('DomainFlag')
+
+    REQUIRED_FIELDS = ('FQDN', 'date_created', 'date_expires', 'date_deletion')
+
+    def __str__(self):
+        return self.FQDN
--- domains/views.py
+++ domains/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Domain, DomainFlag
 
-# Create your views here.
+
+# List view for all the Domains registered
+class DomainListView(ListView):
+    """Lists all Domains"""
+    model = Domain
+    template_name = 'domains.html'
+    context_object_name = 'domains'
+    ordering = ['date_created']
+
+
+class DomainDetailView(DetailView):
+    """Lists detail data of a Domain"""
+    model = Domain
+    template_name = 'domains-detail.html'
+    ordering = ['date_created']
